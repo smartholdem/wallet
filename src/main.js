bfdbde47a0cb85a3e9be6e38cbd7cae61fd78f25
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-15 23:33:28 +0300

    main

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import RouterPrefetch from "vue-router-prefetch";
+import DashboardPlugin from "./plugins/dashboard-plugin";
+import App from "./App.vue";
+
+// router setup
+import router from "./routes/router";
+import i18n from "./i18n";
+import "./registerServiceWorker";
+// plugin setup
+Vue.use(DashboardPlugin);
+Vue.use(VueRouter);
+Vue.use(RouterPrefetch);
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+  router,
+  i18n,
+});
